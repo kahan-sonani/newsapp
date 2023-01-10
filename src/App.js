c55fa30b7fe906e050ae2404828991c8f5428759
kahan-sonani <kahansonani1008@gmail.com>
2023-01-10 15:13:58 +0530

    v0.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 
-import React, { Component, useState } from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 
